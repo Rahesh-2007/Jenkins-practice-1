6a6e3b20850185d44ec3d9a49561348cafb2f960
Rahesh-2007 <rakesh27870@gmail.com>
2026-07-29 16:25:28 +0530

    Hello

--- AddingTwoNum.java
+++ AddingTwoNum.java
@@ -1,7 +1,9 @@
 import java.util.*;
 public class AddingTwoNum{
     public static void main(String[]args){
-        int x=10,y=20;
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
         System.out.printf("Total Sum: %d",x+y);
     }
 }
